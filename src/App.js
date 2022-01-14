2116affa44bbb032223b9c86fa71312839e26a4e
종박 <78944379+jongpark1234@users.noreply.github.com>
2022-01-14 20:15:31 +0900

    6.0 ~ 6.4
    useEffect, Deps

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import Button from "./Button"
-import styles from "./App.module.css"
+import { useState, useEffect } from "react";
+
+function Hello() {
+  useEffect(() => {
+    console.log("Hi")
+    return () => console.log("Bye")
+  })
+  return <h1>hello</h1>
+}
 
 function App() {
+  const [showing, setShowing] = useState(false)
+  const onClick = () => setShowing((prev) => !prev)
   return (
     <div>
-      <h1 className = {styles.title}>Welcome back to React!</h1>
-      <Button text = {"Continue"}/>
+      {showing ? <Hello/> : null}
+      <button onClick={onClick}>{showing ? "hide" : "show"}</button>
     </div>
-  );
+  )
 }
 export default App;
